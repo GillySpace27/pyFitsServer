9bdbbed5de710f10584b35213b17b34ce4b16e91
gillyspace27 <gilly@swri.org>
2024-11-26 22:42:07 -0700

    fix pathing

--- pyfitsserver/color_tables.py
+++ pyfitsserver/color_tables.py
@@ -27,9 +27,11 @@ cmap_data_dir = os.path.join(
     pathlib.Path(__file__).parent.absolute(), "../../data", "idl_3.csv"
 )
 if not os.path.exists(cmap_data_dir):
-    cmap_data_dir = "idl_3.csv"
+    cmap_data_dir = "./idl_3.csv"
 if not os.path.exists(cmap_data_dir):
     cmap_data_dir = "pyfitsserver/idl_3.csv"
+if not os.path.exists(cmap_data_dir):
+    cmap_data_dir = "pyFitsServer/pyfitsserver/idl_3.csv"
 
 assert os.path.exists(cmap_data_dir), f"Could not find cmap_data_dir: {cmap_data_dir}"
 
